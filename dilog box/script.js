50c0a408bb5cd64adc8e25c04c3d787c43f3e094
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-09-11 23:23:29 +0530

    this is revision javaScript

---
+++ dilog box/script.js
@@ -0,0 +1,6 @@
+// alert("This is a alert")
+
+// confirm("Would you like to proceed ")
+
+let name = prompt("Enter Your Name")
+console.log(name);
---
+++ dtaatype/script.js
@@ -0,0 +1,21 @@
+
+// string to number
+
+typeof +'100'
+'number'
+
+// Boolean
+typeof true 
+typeof false
+
+// undefined
+undefined
+
+// null
+null [Object]
+
+// BigInt
+
+
+// Symbol
+// $&@
