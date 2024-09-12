9fc653f6f0f876d24fc3112d9caabc29314cf2dc
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-09-13 02:33:08 +0530

    math in java script

---
+++ math object/script.js
@@ -0,0 +1,59 @@
+let number = 2**2
+console.log(number);
+
+Math.SQRT2
+
+// pi value
+Math.PI
+
+// squreroot
+Math.sqrt(4)
+
+// power
+Math.pow(2, 6)
+
+//remove decemal number but there are limitation
+Math.floor(2.99)
+2
+
+Math.ceil(2.77)
+3
+
+//round of the number
+Math.round(2.6)
+3
+Math.round(2.2)
+2
+
+//For random number generate ( its give 0-1 random number)
+Math.random()
+0.3558140530324183
+
+Math.random() * 20
+10.441455407528286
+
+Math.random() * 20
+8.323816581219173
+
+Math.random() * 20
+11.994038733376392
+//remove decemal number from the random number
+Math.floor(Math.random()*20)
+15
+Math.floor(Math.random()*20)
+8
+
+
+const a = 10;
+const b = 20;
+// Math. random() * (max - min) + min
+const n = Math.floor(Math. random() * (b - a) )+ a
+
+console.log(n);
+
+
+const height = +prompt("Enter the height")
+const width = +prompt("enter the width")
+
+area = +height * +width
+console.log(area);
