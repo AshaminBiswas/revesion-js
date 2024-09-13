cb2c56f01d9cde5918de41b96d6f771c679bb995
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-09-14 02:28:14 +0530

    and operator or operator

---
+++ logical operator/script.js
@@ -0,0 +1,18 @@
+
+//And (&&) operator expression
+// const userAge = 25;
+// (userAge >= 18) && (userAge <= 25)
+// // true
+
+// (userAge >= 18) && (userAge <= 24)
+// // false
+
+
+//Or (||) operators Expressions
+const age = 15;
+const schoolStudent = age >= 5 && age <= 18 //(true)
+// true
+const collegeStudent = age >= 18 && age <= 24 //(false)
+// false
+
+const isStudent = schoolStudent || collegeStudent  //(true)
