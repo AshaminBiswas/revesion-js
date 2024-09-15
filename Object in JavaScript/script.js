07f296c9a0576af66ed1d5b0b02f7aae8df51a5a
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-09-16 01:16:33 +0530

    object in java script

---
+++ Object in JavaScript/script.js
@@ -0,0 +1,32 @@
+//Object Syntax
+
+// const user = {
+//     firstName : "Ashamin",
+//     lastName : "Biswas",
+//     age : 23,
+//     education : "B.tech"
+// }
+
+// user.age = 22
+// user["is-student"] = true
+
+
+const userDetails ={
+    firstName: "Ashamin",
+    lastName: "Biswas",
+    age: 22,
+    gender: "Male",
+    address:{
+        village: "Ahammadpur",
+        postOffice: "Sanmatinagar",
+        policeStation: "Raghunathganj",
+        district: "Murshidabad",
+        pin: 742213,
+        state: "West Bengal",
+        moreDetails:{
+            city: "Jangipur",
+            street:"Saidapur",
+        }
+    }
+
+}
