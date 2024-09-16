7605f9fc0c981c72813b5619e4fc3fcc5d590abd
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-09-17 02:08:39 +0530

    counter app

--- practice.js
+++ practice.js
@@ -1,8 +1,63 @@
-const array = [1,2,3,4,5]
-let sum  = 0
-for (let index = 0; index < array.length; index++) {
-    
-    sum += array[index]
-    
+let count = 0
+
+function updateCounter(){
+    const value = document.querySelector(".value").innerText = count
 }
-console.log(sum)
+
+const addBtn = document.querySelector(".addbutton")
+const subtractButton = document.querySelector(".subtract")
+
+addBtn.addEventListener("click",() =>{
+    count ++
+    updateCounter()
+})
+subtractButton.addEventListener("click", () =>{
+    if(count > 0)(
+        count--
+    )
+    updateCounter()
+})
+
+
+
+
+
+
+// const array = [1,2,3,4,5]
+// let sum  = 0
+// for (let index = 0; index < array.length; index++) {
+
+//     sum += array[index]
+
+// }
+// console.log(sum)
+
+
+
+
+
+const user1 ={
+    name:"john",
+    age:25,
+    address:{
+        city:"mumbai",
+        state:"maharastra",
+    }
+}
+
+const user2 = user1
+
+user2.name ="ashamin"
+user2.address.city = "pune",
+
+console.log(user1)
+console.log(user2);
+
+
+
+
+// for (let i = 0; i <= 3; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
