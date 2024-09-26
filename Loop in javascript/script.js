8fc240d5c75ac4937134cfea159b9b0a2e1792dc
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-09-26 22:25:35 +0530

    while loop

---
+++ Loop in javascript/script.js
@@ -0,0 +1,20 @@
+//While loop
+
+
+// let num = 1
+// while (num <= 100){
+//     // console.log("while loop");
+//     console.log(num);
+//     num = num + 1
+// }
+
+
+let friend = ["soumen", "bala", "hasan", "payrada", "ashamin"]
+let i = 0
+while (i < friend.length) {
+    console.log(` ${i + 1}. ${friend[i]}`);
+    friend[i] = friend[i] + " TIB"
+    i++ 
+}
+
+//while loop complete
