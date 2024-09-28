6e60bfc944fb9504794ad520b905932060633398
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-09-28 11:20:50 +0530

    for loop and do while loop

---
+++ Loop in javascript/forloop.js
@@ -0,0 +1,22 @@
+
+for(let i = 0; i < 10; i++){
+    console.log(i);  
+}
+console.log("loop ended");
+
+const friends = ["soumen", "hayat", "hasan", "asik"]
+
+for(let i = 0; i < friends.length; i++){
+    console.log(friends[i]);
+        friends[i] = friends[i] + " tib"
+}
+
+// print all the even numbers
+
+for(let num = 0; num <100; num++){
+    if(num % 2 == 0){
+        console.log(num + 2);
+        
+    }
+    
+}
