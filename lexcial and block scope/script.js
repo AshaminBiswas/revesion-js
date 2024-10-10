2950b52975b167b630d3ab1e41c69422619f4d34
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-10-11 03:50:44 +0530

    lexical and block scope

---
+++ lexcial and block scope/script.js
@@ -0,0 +1,14 @@
+//Local scope
+
+function add(){
+    const a = 2;
+    const b = 5;
+    console.log(a + b);
+
+    function myName(){
+        const name = "ASHAMIN"
+        console.log(name);
+        
+    }
+    myName()
+}
