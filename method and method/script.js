7be69f085a42a8de240d8171f40a420ea6b086bd
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-10-20 17:34:26 +0530

    every function is a method but every function is not a method

---
+++ method and method/script.js
@@ -0,0 +1,13 @@
+const math = {
+    square:function(a){
+        return a**2
+    },
+
+    add:function(a, b){
+        return a + b;
+    },
+
+    subtract(a, b){
+        return a - b
+    }
+}
