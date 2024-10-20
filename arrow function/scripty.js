733460b4a4d9a40a5ca88f29ea0db92bff59f61e
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-10-20 17:54:13 +0530

    arrow function with Implicit return

---
+++ arrow function/scripty.js
@@ -0,0 +1,17 @@
+function square(num){
+    return num * num;
+}
+
+// function expression
+const sqrt = function(num){
+    return num * num
+}
+
+// arrow function 
+//explicit return
+const sqr = (num)=>{
+    return num + num
+}
+
+//implicit return
+const add = (num1, num2)=> num1 + num2
