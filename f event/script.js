5f4148ff7fe272b3c133680389bc40df8355c57f
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-11-15 11:16:01 +0530

    form event

---
+++ f event/script.js
@@ -0,0 +1,56 @@
+const userInput = document.querySelector("#user-input")
+
+// userInput.addEventListener("click", ()=>{
+//     console.log("clicked");
+    
+// })
+
+
+
+
+
+//INPUT EVENT
+
+// userInput.addEventListener("input", (e)=>{
+//     console.log(e.target.value);
+    
+// })
+
+
+//CHANGE (when change the value in side a input)
+// userInput.addEventListener("change", (e)=>{
+//     console.log(e.target.value);
+    
+// })
+
+
+
+//FOCUS 
+// userInput.addEventListener("focus", (e)=>{
+//     console.log(e.target.value);
+    
+// })
+
+
+
+//BLUR
+// userInput.addEventListener("focus", (e)=>{
+//     console.log(e.target.value);
+    
+// })
+
+
+
+
+//Submit
+const form = document.querySelector("form")
+// form.addEventListener("submit",()=>{
+//     e.preventDefault()
+//     console.log(e);
+//     const myFormData = new FormData(form)
+//     for(const p of myFormData.entries()){
+//         console.log(p);
+        
+//     }
+    
+// })
