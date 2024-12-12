821e3410919691564476206821817fc66c162e01
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-12-13 03:25:37 +0530

    xml http request

---
+++ XHR/script.js
@@ -0,0 +1,17 @@
+
+const button = document.querySelector("#button")
+const img = document.querySelector("img")
+button.addEventListener("click",()=>{
+    const xhr = new XMLHttpRequest()
+    console.log(xhr);
+
+    xhr.addEventListener("load", ()=>{
+        img.src = xhr.response.message
+        console.log(JSON.parse(xhr.response));
+        
+    })
+
+    xhr.open("GET", "https://dog.ceo/api/breeds/image/random")
+    xhr.send()
+    
+})
