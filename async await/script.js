312820cef8855ba04f7b8bc75f282accc66d6314
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-12-20 10:40:32 +0530

    async function

---
+++ async await/script.js
@@ -0,0 +1,10 @@
+async function makeAsyncRequest() {
+    const url = 'https://dummyjson.com/test'
+    const response = await fetch(url)
+    console.log(response);
+    
+    // const data = await response.json()
+    // return data
+}
+makeAsyncRequest()
+
