4917db429077469b098fc0c189a0b48babf3f39c
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-02-03 15:22:27 +0530

    done

--- async await/script.js
+++ async await/script.js
@@ -1,10 +1,10 @@
 async function makeAsyncRequest() {
-    const url = 'https://dummyjson.com/test'
-    const response = await fetch(url)
-    console.log(response);
+    // const url = 'https://dummyjson.com/test'
+    // const response = await fetch(url)
+    // console.log(response);
     
-    // const data = await response.json()
-    // return data
+    // // const data = await response.json()
+    // // return data
 }
 makeAsyncRequest()
 
