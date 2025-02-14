f0df8fbcfdc78b61fa44994dfdd97c9f1cf7d241
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-02-14 15:21:31 +0530

    done

--- async/script.js
+++ async/script.js
@@ -1,9 +1,21 @@
-async function request() {
-    const url = 'https://6wrlmkp9u2.execute-api.us-east-1.amazonaws.com'
-    const response = fetch(url)
-    response.then((response)=>{
-        console.log(response);
+// async function request() {
+    
+//     const url = 'https://6wrlmkp9u2.execute-api.us-east-1.amazonaws.com'
+//     const response = fetch(url)
+//     response.then((response)=>{
+//         console.log(response);
         
-    })
-}
-request()
+//     })
+// }
+// request()
+
+
+const thread = document.querySelector(".block")
+thread.addEventListener("click",()=>{
+    const startTime = Date.now()
+    let currentTime = startTime
+    while(startTime + 4000 > currentTime){
+        currentTime = Date.now()
+    }
+})
+
